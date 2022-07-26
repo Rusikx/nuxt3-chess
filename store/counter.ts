f8519157fda0fde981fc5aa3898dedb0068839c2
Liliya <liliyavoloshine@gmail.com>
2022-07-26 21:29:32 +0600

    chore: pinia simple setup

---
+++ store/counter.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+
+interface State {
+  count: number
+}
+
+export const useCounterStore = defineStore({
+  id: 'counter-store',
+  state: (): State => {
+    return {
+      count: 0
+    }
+  },
+  actions: {
+    multiple() {
+      this.count = this.count * 2
+    }
+  },
+  getters: {
+    actualCount: state => state.count,
+    doubleCount() {
+      return this.count * 2
+    }
+  }
+})
