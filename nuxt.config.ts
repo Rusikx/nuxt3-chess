b3992dfdc3dd55b43903ac9a2f7ff638b78ab336
Rusikx <zrusikxz@gmail.com>
2022-09-14 12:22:40 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   router: {
-    base: '/app'
+    base: '/'
   },
   buildModules: ['@pinia/nuxt'],
   vite: {
